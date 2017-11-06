93cea2f02851d79200ce144f37e88c65dc198a44
singhnsatya <singhnsatya@gmail.com>
2017-11-06 15:02:03 +0530

    adding cors and connection changes

--- python-api/app.py
+++ python-api/app.py
@@ -1,6 +1,4 @@
 from helpers.server import app
-from helpers.connection import connectDB
 
 if __name__ == '__main__':
-	connectDB()
 	app.run(debug=True)
--- python-api/helpers/server.py
+++ python-api/helpers/server.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request
 from routes.urls import routes
+from helpers.connection import connectDB
+from flask_cors import CORS
 import psycopg2
 import json
 
 app = Flask(__name__)
-
+connectDB()
+CORS(app)
 app.register_blueprint(routes)
